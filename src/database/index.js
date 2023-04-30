26b73c00da1c64518ee15323f62dc5f9bbbbf288
Kauã <kaua.borba03@gmail.com>
2023-04-30 15:25:24 -0300

    test

--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,11 @@
-try{
-  const Sequelize = require('sequelize');
-  const databaseConfig = require('../config/database');
-  const User = require('../models/User');
-  const Post = require('../models/Post');
+const Sequelize = require('sequelize');
+const databaseConfig = require('../config/database');
+const User = require('../models/User');
+const Post = require('../models/Post');
 
-  const models = [User, Post];
+const models = [User, Post];
 
-  const connection = new Sequelize(databaseConfig);
+const connection = new Sequelize(databaseConfig);
 
-  models.forEach((model) => model.init(connection));
-  models.forEach((model) => model.associate && model.associate(connection.models));
-}catch(e){
-  console.error(e);
-}
+models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
