f6cc4d3109fad7157d3898c1ad1f226038314048
Kauã <kaua.borba03@gmail.com>
2023-05-03 03:23:38 -0300

    finish

--- api/index.js
+++ api/index.js
@@ -1,29 +1,25 @@
-try{
-  require('../src/database');
-  require('dotenv').config();
-  const express = require('express');
-  const cors = require('cors');
-  const { resolve } = require('path');
+require('../src/database');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { resolve } = require('path');
 
-  const app = express();
+const app = express();
 
-  const userRoutes = require('../src/routes/user');
-  const tokenRoutes = require('../src/routes/token');
-  const postRoutes = require('../src/routes/post');
+const userRoutes = require('../src/routes/user');
+const tokenRoutes = require('../src/routes/token');
+const postRoutes = require('../src/routes/post');
 
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(resolve(__dirname, 'uploads')));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(resolve(__dirname, '..','uploads')));
 
-  app.use('/users', userRoutes);
-  app.use('/tokens', tokenRoutes);
-  app.use('/posts', postRoutes);
+app.use('/users', userRoutes);
+app.use('/tokens', tokenRoutes);
+app.use('/posts', postRoutes);
 
-  app.listen(process.env.PORT, () =>{
-    console.log('Server is running...');
-  });
+app.listen(process.env.PORT, () =>{
+  console.log('Server is running...');
+});
 
-}catch(e){
-  console.log(e);
-}
