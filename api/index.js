6b23411d1f288c5e07257c3e90b70c571c1d0ccb
Kauã <kaua.borba03@gmail.com>
2023-05-02 22:43:48 -0300

    vercel

--- api/index.js
+++ api/index.js
@@ -4,23 +4,12 @@ try{
   const express = require('express');
   const cors = require('cors');
   const { resolve } = require('path');
-  const User = require('../src/models/User');
 
   const app = express();
 
   const userRoutes = require('../src/routes/user');
   const tokenRoutes = require('../src/routes/token');
   const postRoutes = require('../src/routes/post');
-
-  app.get('/', async (req, res) =>{
-    try{
-      const users = await User.findAll();
-
-      return res.json(users);
-    }catch(e){
-      res.json(e);
-    }
-  });
 
   app.use(cors());
   app.use(express.json());
